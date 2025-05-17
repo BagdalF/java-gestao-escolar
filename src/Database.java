c8e8b52af22a30e0b5eec876d016268f688dfdbc
BagdalF <bagdalfelipe@gmail.com>
2025-05-17 16:50:11 -0300

    Utilização do Factory Method

--- src/Database.java
+++ src/Database.java
@@ -11,12 +11,12 @@ public class Database {
             List<Turma> turmas) {
 
         // Populando professores
-        Professor professor1 = new Professor("João Silva", "12345678900", "joao@escola.com", "123456789", 1);
-        Professor professor2 = new Professor("Maria Oliveira", "98765432100", "maria@escola.com", "987654321", 2);
-        Professor professor3 = new Professor("Pedro Santos", "33344455566", "pedro@escola.com", "333444555", 3);
-        Professor professor4 = new Professor("Clara Mendes", "44455566677", "clara@escola.com", "444555666", 4);
-        Professor professor5 = new Professor("Lucas Almeida", "55566677788", "lucas@escola.com", "555666777", 5);
-        Professor professor6 = new Professor("Fernanda Costa", "66677788899", "fernanda@escola.com", "666777888", 6);
+        Professor professor1 = EntityFactory.criarProfessor("João Silva", "12345678900", "joao@escola.com", "123456789", 1);
+        Professor professor2 = EntityFactory.criarProfessor("Maria Oliveira", "98765432100", "maria@escola.com", "987654321", 2);
+        Professor professor3 = EntityFactory.criarProfessor("Pedro Santos", "33344455566", "pedro@escola.com", "333444555", 3);
+        Professor professor4 = EntityFactory.criarProfessor("Clara Mendes", "44455566677", "clara@escola.com", "444555666", 4);
+        Professor professor5 = EntityFactory.criarProfessor("Lucas Almeida", "55566677788", "lucas@escola.com", "555666777", 5);
+        Professor professor6 = EntityFactory.criarProfessor("Fernanda Costa", "66677788899", "fernanda@escola.com", "666777888", 6);
 
         professores.add(professor1);
         professores.add(professor2);
@@ -27,12 +27,12 @@ public class Database {
 
         // Populando disciplinas
 
-        Disciplina disciplina1 = new Disciplina(1, "Matemática", "Matemática básica", 80);
-        Disciplina disciplina2 = new Disciplina(2, "Português", "Gramática e literatura", 60);
-        Disciplina disciplina3 = new Disciplina(3, "História", "História geral", 100);
-        Disciplina disciplina4 = new Disciplina(4, "Geografia", "Geografia física e política", 60);
-        Disciplina disciplina5 = new Disciplina(5, "Física", "Física básica", 40);
-        Disciplina disciplina6 = new Disciplina(6, "Química", "Química geral", 80);
+        Disciplina disciplina1 = EntityFactory.criarDisciplina(1, "Matemática", "Matemática básica", 80);
+        Disciplina disciplina2 = EntityFactory.criarDisciplina(2, "Português", "Gramática e literatura", 60);
+        Disciplina disciplina3 = EntityFactory.criarDisciplina(3, "História", "História geral", 100);
+        Disciplina disciplina4 = EntityFactory.criarDisciplina(4, "Geografia", "Geografia física e política", 60);
+        Disciplina disciplina5 = EntityFactory.criarDisciplina(5, "Física", "Física básica", 40);
+        Disciplina disciplina6 = EntityFactory.criarDisciplina(6, "Química", "Química geral", 80);
         
         disciplinas.add(disciplina1);
         disciplinas.add(disciplina2);
@@ -50,14 +50,14 @@ public class Database {
         disciplina6.adicionarProfessor(professor6);
 
         // Populando estudantes
-        Estudante estudante1 = new Estudante("Carlos Souza", "11122233344", "carlos@escola.com", "111222333", 1, null, new ArrayList<>());
-        Estudante estudante2 = new Estudante("Ana Lima", "55566677788", "ana@escola.com", "555666777", 2, null, new ArrayList<>());
-        Estudante estudante3 = new Estudante("Bruno Ferreira", "22233344455", "bruno@escola.com", "222333444", 3, null, new ArrayList<>());
-        Estudante estudante4 = new Estudante("Mariana Silva", "33344455566", "mariana@escola.com", "333444555", 4, null, new ArrayList<>());
-        Estudante estudante5 = new Estudante("Renato Lima", "44455566677", "renato@escola.com", "444555666", 5, null, new ArrayList<>());
-        Estudante estudante6 = new Estudante("Juliana Costa", "55566677788", "juliana@escola.com", "555666777", 6, null, new ArrayList<>());
-        Estudante estudante7 = new Estudante("Gabriel Nunes", "66677788800", "gabriel@escola.com", "666777888", 7, null, new ArrayList<>());
-        Estudante estudante8 = new Estudante("Larissa Rocha", "77788899911", "larissa@escola.com", "777888999", 8, null, new ArrayList<>());
+        Estudante estudante1 = EntityFactory.criarEstudante("Carlos Souza", "11122233344", "carlos@escola.com", "111222333", 1, null, new ArrayList<>());
+        Estudante estudante2 = EntityFactory.criarEstudante("Ana Lima", "55566677788", "ana@escola.com", "555666777", 2, null, new ArrayList<>());
+        Estudante estudante3 = EntityFactory.criarEstudante("Bruno Ferreira", "22233344455", "bruno@escola.com", "222333444", 3, null, new ArrayList<>());
+        Estudante estudante4 = EntityFactory.criarEstudante("Mariana Silva", "33344455566", "mariana@escola.com", "333444555", 4, null, new ArrayList<>());
+        Estudante estudante5 = EntityFactory.criarEstudante("Renato Lima", "44455566677", "renato@escola.com", "444555666", 5, null, new ArrayList<>());
+        Estudante estudante6 = EntityFactory.criarEstudante("Juliana Costa", "55566677788", "juliana@escola.com", "555666777", 6, null, new ArrayList<>());
+        Estudante estudante7 = EntityFactory.criarEstudante("Gabriel Nunes", "66677788800", "gabriel@escola.com", "666777888", 7, null, new ArrayList<>());
+        Estudante estudante8 = EntityFactory.criarEstudante("Larissa Rocha", "77788899911", "larissa@escola.com", "777888999", 8, null, new ArrayList<>());
 
         estudantes.add(estudante1);
         estudantes.add(estudante2);
@@ -72,17 +72,17 @@ public class Database {
         List<Disciplina> disciplinasTurma1 = new ArrayList<>();
         disciplinasTurma1.add(disciplina1);
         disciplinasTurma1.add(disciplina2);
-        Turma turma1 = new Turma(1, "Turma Fundamentos", disciplinasTurma1);
+        Turma turma1 = EntityFactory.criarTurma(1, "Turma Fundamentos", disciplinasTurma1);
 
         List<Disciplina> disciplinasTurma2 = new ArrayList<>();
         disciplinasTurma2.add(disciplina3);
         disciplinasTurma2.add(disciplina4);
-        Turma turma2 = new Turma(2, "Turma Humanas", disciplinasTurma2);
+        Turma turma2 = EntityFactory.criarTurma(2, "Turma Humanas", disciplinasTurma2);
 
         List<Disciplina> disciplinasTurma3 = new ArrayList<>();
         disciplinasTurma3.add(disciplina5);
         disciplinasTurma3.add(disciplina6);
-        Turma turma3 = new Turma(3, "Turma Exatas", disciplinasTurma3);
+        Turma turma3 = EntityFactory.criarTurma(3, "Turma Exatas", disciplinasTurma3);
 
         turmas.add(turma1);
         turmas.add(turma2);
